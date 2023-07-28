4e92b4df537572e1d52025cb537b7e8b96e6b2be
Michele Ferrari <m.ferrari092@studenti.unibs.it>
2023-07-28 11:10:10 +0200

    filtering completed and working

--- src/Inxpect.py
+++ src/Inxpect.py
@@ -37,9 +37,10 @@ class Inxpect:
     sensor_data_preproc: np.array = field(default=None, init=False)
     sensor_data_filtered: np.array = field(default=None, init=False)
     no_signal_samples: int = field(default=0, init=False)
-    filter_win_length: int = field(default=100, init=True)
+    filter_win_length: int = field(default=100, init=True) # window for moving-average filtering
     n_cycles: int = field(default=0, init=False)
-    ignore_win_length: int = field(default=10, init=True)
+    ignore_win_length: int = field(default=50, init=True) # window for null outlier rejection
+    is_detecting: bool = field(default=False, init=True) # is the sensor detecting the human along the current window?
 
 
     def __post_init__(self):
@@ -77,9 +78,14 @@ class Inxpect:
                 self.sensor_data_preproc = np.copy(self.sensor_data)
             
             else:
+                # Slide window
                 self.sensor_data[:,:-1] = self.sensor_data[:,1:]
+                self.sensor_data_preproc[:,:-1] = self.sensor_data_preproc[:,1:]
+
+                # Add new raw sample
                 self.sensor_data[:,-1] = sensor_data_new
 
+                # Get self.ignore_win_length latest raw samples
                 latest_data = self.sensor_data[:,-self.ignore_win_length:]
 
                 print("Latest data in window: ")
@@ -87,35 +93,39 @@ class Inxpect:
 
                 if self.human_detected(latest_data):
                     print("Person detected", end="\n")
-                    
-                    # for i in range(0,N_SENSOR_DATA): # remove unexpected zeros
-                    #     null_condition = (latest_data[i] < 1e-3)
-                    #     idx_zero = np.where(null_condition)[0]
-                    #     idx_not_zero = np.where(np.invert(null_condition))[0]
 
-                    #     print("Measurement #" + str(i) + " before processing: " + str(latest_data[i]), end="\n")
-                    #     if len(idx_not_zero.tolist()) != 0 and len(idx_zero.tolist()) != 0:
-                    #         latest_data[i][idx_zero] = np.mean(latest_data[i][idx_not_zero])
-                    #     print("Measurement #" + str(i) + " after processing: " + str(latest_data[i]), end="\n\n")
+                    # Add new preprocessed sample
+                    self.sensor_data_preproc[:,-1] = sensor_data_new
 
-                    # self.sensor_data_preproc[:,-self.ignore_win_length:] = latest_data
+                    # Activate filtering after a given number of cycles (to fill the whole filtering window)
+                    if self.n_cycles > self.filter_win_length:
+                        self.remove_zeros(latest_data)
 
-                    # self.moving_average_filter() # filter data
+                    # Filter data using moving-average filter                   
+                    self.moving_average_filter()
+
+                    # Filter data using either state-observer or first-order LPF
+                    # if not self.state_obs_initialized:
+                    #     self.initialize_state_observer(sensor_data_new)
+                    # else:
+                    #     self.update_state_observer(sensor_data_new)
+                    #     # self.first_order_filter(sensor_data_new)
+
+                    self.is_detecting = True
 
                 else:
                     print("Person NOT detected", end="\n\n")
-                    self.sensor_data_preproc[:,-self.ignore_win_length:] = np.zeros((N_SENSOR_DATA,self.ignore_win_length),dtype=float)
 
-        # if not self.state_obs_initialized:
-        #     self.initialize_state_observer(sensocr_data_new)
-        # else:
-        #     # if distance < 0.1 and self.no_signal_samples < 300:
-        #     #     sensor_data_new = self.sensor_data
-        #     #     self.no_signal_samples += 1
-        #     # else:
-        #     #     self.no_signal_samples = 0
-        #     self.update_state_observer(sensor_data_new)
-        #     # self.first_order_filter(sensor_data_new)
+                    # Ignore new sample from sensor data
+                    if self.is_detecting: # if the sensor was detecting, keep the latest measured value
+                        self.sensor_data_preproc[:,-1] = self.sensor_data_preproc[:,-2]
+                        self.sensor_data_filtered[:,-1] = self.sensor_data_filtered[:,-2]
+                    else:   # if the sensor was not detecting, add a zero measurement        
+                        self.sensor_data_preproc[:,-1] = np.zeros((N_SENSOR_DATA,),dtype=float)
+                        self.sensor_data_filtered[:,-1] = np.zeros((N_SENSOR_DATA,),dtype=float)
+                    
+                    if self.count_detections(latest_data) == 0:
+                        self.is_detecting = False
 
         return self.sensor_data[0,-1], self.sensor_data[1,-1], \
                self.sensor_data_preproc[0,-1], self.sensor_data_preproc[1,-1], \
@@ -155,14 +165,33 @@ class Inxpect:
         a = np.ones(1)  #denominator co-effs of filter transfer function
         
         for i in range(0,N_SENSOR_DATA):
-            self.sensor_data_filtered[i] = signal.lfilter(b,a,self.sensor_data_preproc[i,:]) #filter output using lfilter function
+            self.sensor_data_filtered[i,-self.filter_win_length:] = \
+                signal.lfilter(b,a,self.sensor_data_preproc[i,-self.filter_win_length:]) # filter output using lfilter function
 
 
     def human_detected(self, data):
+        n_dect = self.count_detections(data)
+        print("Number of detections in window: " + str(n_dect))
+        return n_dect == self.ignore_win_length
+    
+
+    def count_detections(self, data):
         n_dect = 0
         for i in range(0,data.shape[1]):
             if np.any(np.abs(data[:,i]) > 1e-3):
                 n_dect +=1
-        print("Number of detection in window: " + str(n_dect))
+        return n_dect
+    
 
-        return n_dect > 0.5*self.ignore_win_length
+    def remove_zeros(self, data):
+        for i in range(0,N_SENSOR_DATA): # remove unexpected zeros
+            null_condition = (data[i] < 1e-3)
+            idx_zero = np.where(null_condition)[0]
+            idx_not_zero = np.where(np.invert(null_condition))[0]
+
+            print("Measurement #" + str(i) + " before processing: " + str(data[i]), end="\n")
+            if len(idx_not_zero.tolist()) != 0 and len(idx_zero.tolist()) != 0:
+                data[i][idx_zero] = np.mean(data[i][idx_not_zero])
+            print("Measurement #" + str(i) + " after processing: " + str(data[i]), end="\n\n")
+
+        self.sensor_data_preproc[:,-self.ignore_win_length:] = data
